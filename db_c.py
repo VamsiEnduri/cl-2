836e1ceefaef4790e1a64e9e970947fa1b7ccfdc
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-19 12:20:35 +0530

    signupand logon and dashboarda nd upload files

--- db_c.py
+++ db_c.py
@@ -1,44 +1,3 @@
-# import streamlit as st
-# import mysql.connector
-
-# conn = mysql.connector.connect(
-#     host=st.secrets["DB_HOST"],
-#     user=st.secrets["DB_USER"],
-#     password=st.secrets["DB_PASSWORD"],
-#     database=st.secrets["DB_NAME"],
-#     port=st.secrets["DB_PORT"]
-# )
-
-# cursor = conn.cursor(dictionary =True)
-
-# # USERS TABLE
-# cursor.execute("""
-# CREATE TABLE IF NOT EXISTS users(
-#     id INT PRIMARY KEY AUTO_INCREMENT,
-#     name VARCHAR(100),
-#     email VARCHAR(100) UNIQUE,
-#     password VARCHAR(100)
-# )
-# """)
-
-# # FILES TABLE
-# cursor.execute("""
-# CREATE TABLE IF NOT EXISTS files(
-#     id INT PRIMARY KEY AUTO_INCREMENT,
-#     user_id INT,
-#     file_name VARCHAR(255),
-#     file_type VARCHAR(100),
-#     file_url TEXT,
-#     upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-#     FOREIGN KEY(user_id) REFERENCES users(id)
-# )
-# """)
-
-# conn.commit()
-
-# print("Tables Created Successfully")
-
-
 
 import mysql.connector
 import streamlit as st
@@ -52,3 +11,32 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor(dictionary=True)
+
+
+
+# USERS TABLE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(100)
+)
+""")
+
+# FILES TABLE
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS files(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    file_name VARCHAR(255),
+    file_type VARCHAR(100),
+    file_url TEXT,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+""")
+
+conn.commit()
+
+print("Tables Created Successfully")
